5950bc6c75af4b6593435ca55851d793d9203cd6
srvsngh200892 <srvsngh200892@gmail.com>
2017-06-22 01:53:40 +0530

    changes in sidekiq.yml and procfile

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,18 +1,9 @@
 require 'sidekiq'
-require 'sidekiq-status'
-require 'sidekiq/api'
 
 Sidekiq.configure_client do |config|
-  config.client_middleware do |chain|
-    chain.add Sidekiq::Status::ClientMiddleware
-  end
+  config.redis = { :size => 1 }
 end
 
 Sidekiq.configure_server do |config|
-  config.server_middleware do |chain|
-    chain.add Sidekiq::Status::ServerMiddleware, expiration: 30.minutes # default
-  end
-  config.client_middleware do |chain|
-    chain.add Sidekiq::Status::ClientMiddleware
-  end
+  config.redis = { :size => 2 }
 end
