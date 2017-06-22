99d4d168a0465eee9b2fa42bfe1c40ae3322cdf5
srvsngh200892 <srvsngh200892@gmail.com>
2017-06-22 08:23:37 +0530

    schema in model file

--- app/models/object_state.rb
+++ app/models/object_state.rb
@@ -1,3 +1,20 @@
+# == Schema Information
+#
+# Table name: object_states
+#
+#  id                              :integer          not null, primary key
+#  object_id                       :integer
+#  object_type                     :string(255)
+#  object_changes                  :json
+#  timestamp                       :binint
+#  created_at                      :datetime
+#  updated_at                      :datetime
+#
+# Indexes
+#
+#  index_object_states_on_object_states_object_changes  (object_changes, object_type)
+#
+
 class ObjectState < ActiveRecord::Base
 
   validates :object_id, :object_type, :timestamp, presence: true
